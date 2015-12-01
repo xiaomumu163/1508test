f6f6fccb3b69c4a708e178162c0deafe2827c9a0
hehuan <87313584@qq.com>
2015-12-01 10:06:15 +0800

    test

---
+++ testGitVersion/testGitVersion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGitVersion
+//
+//  Created by 王勇 on 15/12/1.
+//  Copyright © 2015年 wangyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGitVersion/testGitVersion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGitVersion
+//
+//  Created by 王勇 on 15/12/1.
+//  Copyright © 2015年 wangyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
